7faebce5567c2b962435221c653fde8024008e83
sl4vkek <43294802+sl4vkek@users.noreply.github.com>
2019-05-09 13:34:36 +0300

    Update __init__.py

--- ynet/__init__.py
+++ ynet/__init__.py
@@ -4,9 +4,10 @@ from urllib.parse import urlparse
 from datetime import date
 import json
 
+
 class Comment:
     
-    def __init__(self, article_url='' ,name='', email='', location='', title='', text='', likes=0, commentNum=0, commentDate=None, commentId=0, parentId=0):
+    def __init__(self, article_url='', name='', email='', location='', title='', text='', likes=0, commentNum=0, commentDate=None, commentId=0, parentId=0):
         self.article = Article(article_url)
         self.name = name
         self.email = email
@@ -28,22 +29,25 @@ class Comment:
         return replies
 
     def Reply(self, comment):
-        r = requests.get('https://www.ynet.co.il/YediothPortal/Ext/TalkBack/CdaTalkBackTrans/0,2499,'+ \
-                         self.article.article_id +\
-                         '-0-68-13108-0---' + \
-                         str(self.commentId) +\
-                         ',00.html', params={'WSGBRWSR':'FF',\
-                                             'name':comment.name, \
-                                             'email':comment.email, \
-                                             'Location':comment.location, \
-                                             'title':comment.title, \
-                                             'description':comment.text})
+        r = requests.get('https://www.ynet.co.il/YediothPortal/Ext/TalkBack/CdaTalkBackTrans/0,2499,' +
+                         self.article.article_id +
+                         '-0-68-13108-0---' +
+                         str(self.commentId) +
+                         ',00.html', params={'WSGBRWSR':'FF',
+                                             'name': comment.name,
+                                             'email': comment.email,
+                                             'Location': comment.location,
+                                             'title': comment.title,
+                                             'description': comment.text})
         return r.text
     
-    def Post(self, useragent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36"):
+    def Post(self, useragent=\
+            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) \
+AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36"):
         r = requests.post(self.article.article_url, params={'WSGBRWSR':'FF', 'name':self.name,\
         'email':self.email, 'Location':self.location, 'title':self.title, 'description':self.text}, headers={'User-Agent':useragent})
         return r.text
+
 
 class Article:
 
@@ -54,21 +58,22 @@ class Article:
     def GetComments(self):
         comments = []
         r = requests.get("https://www.ynet.co.il/Ext/Comp/ArticleLayout/Proc/ShowTalkBacksAjax/v2/0,12990,"+ self.article_id + "-desc-68-0-1,00.html", params={'User-Agent':\
-                         "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36"}).text
+                         "Mozilla/5.0 (Windows NT 10.0; Win64; x64) \
+AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36"}).text
         comments_json = json.loads(r)['rows']
         for comment_dict in comments_json:
             _comment_date = comment_dict['the_date'].split('.')
             comment_date = date(year=int(_comment_date[2]), month=int(_comment_date[1]), day=int(_comment_date[0]))
-            comments.append(Comment(article_url=self.article_url,\
-                                    name=comment_dict['name'], \
-                                    title=comment_dict['title'], \
-                                    text=comment_dict['text'], \
-                                    location=comment_dict['location'], \
-                                    commentDate=comment_date, \
-                                    commentNum=comment_dict['tc'], \
-                                    likes=comment_dict['ts'], \
-                                    commentId=comment_dict['id'], \
-                                    parentId=comment_dict['parent_id'] \
+            comments.append(Comment(article_url=self.article_url,
+                                    name=comment_dict['name'],
+                                    title=comment_dict['title'],
+                                    text=comment_dict['text'],
+                                    location=comment_dict['location'],
+                                    commentDate=comment_date,
+                                    commentNum=comment_dict['tc'],
+                                    likes=comment_dict['ts'],
+                                    commentId=comment_dict['id'],
+                                    parentId=comment_dict['parent_id']
                                     
                 ))
         return comments     
