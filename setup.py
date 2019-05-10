7189e1337bb015cd37cd5dfa07127ef61818c154
sl4vkek <43294802+sl4vkek@users.noreply.github.com>
2019-05-10 14:56:22 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 
 setup(name='ynet',
-      version='1.3',
+      version='1.4',
       description='ynet.co.il interaction library',
       author='sl4v',
       author_email='iamsl4v@protonmail.com',
