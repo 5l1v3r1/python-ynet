347a349248980403ade0cdcc18ef4229dc0b4a9e
sl4vkek <43294802+sl4vkek@users.noreply.github.com>
2019-06-01 12:17:58 +0300

    1.6.8 update

--- examples/example_post_comment.py
+++ examples/example_post_comment.py
@@ -1,11 +1,12 @@
 from ynet import *
 
-comment = Comment(article_url="https://www.ynet.co.il/articles/0,7340,L-5506634,00.html",\
-                  name='python-ynet bot example',\
-                  email='notyourbusiness@gmail.com',\
-                  location='Example location',\
-                  title='python-ynet bot example',\
-                  text='python-ynet bot example',\
-                  )
+comment = Comment(
+    Article("https://www.ynet.co.il/articles/0,7340,L-5502137,00.html"),
+    name='python-ynet bot example',
+    email='notyourbusiness@gmail.com',
+    location='Example location',
+    title='python-ynet bot example',
+    text='python-ynet bot example',
+)
 
-comment.Post()
+print(comment.Post())
