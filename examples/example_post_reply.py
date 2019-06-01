3be2a2f94caffa3a5b19f449b7f00b495292f6a0
sl4vkek <43294802+sl4vkek@users.noreply.github.com>
2019-06-01 12:23:10 +0300

    Update example_post_reply.py

--- examples/example_post_reply.py
+++ examples/example_post_reply.py
@@ -8,10 +8,12 @@ comment = article.GetCommentByCommentNum(3)
 print('Found comment:' + comment.title)
 print('Comment text:' + comment.text)
 print('Sending reply...')
-reply = Comment(article_url=article.article_url,\
-                name='python-ynet bot reply test',\
-                email='fuckoff@gmail.com', \
-                location='your mom\'s butthole', \
-                title='Test reply using python-ynet', \
-                text='github.com/sl4vkek/python-ynet')
+reply = Comment(
+    article,
+    name='python-ynet bot reply test',
+    email='fuckoff@gmail.com',
+    location='your mom\'s butthole',
+    title='Test reply using python-ynet',
+    text='github.com/sl4vkek/python-ynet'
+    )
 comment.Reply(reply)
